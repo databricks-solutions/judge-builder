dc0490da7008c79090dfecf35b1701154484aa0f
Samraj Moorjani <samraj.moorjani@databricks.com>
2025-09-02 18:00:34 -0400

    Fix: bug in setup script with sed

--- setup.sh
+++ setup.sh
@@ -101,7 +101,7 @@ if [ "$UPDATE_CONFIG" = true ]; then
     
     if [ "$profile" != "$DATABRICKS_CONFIG_PROFILE" ]; then
         # Remove existing line and add new one
-        sed -i '' '/^DATABRICKS_CONFIG_PROFILE=/d' .env.local
+        sed -i '' -e '/^DATABRICKS_CONFIG_PROFILE=/d' .env.local
         echo "DATABRICKS_CONFIG_PROFILE=$profile" >> .env.local
         export DATABRICKS_CONFIG_PROFILE="$profile"
     fi
@@ -122,7 +122,7 @@ if [ "$UPDATE_CONFIG" = true ]; then
     
     if [ "$app_name" != "$DATABRICKS_APP_NAME" ]; then
         # Remove existing line and add new one
-        sed -i '' '/^DATABRICKS_APP_NAME=/d' .env.local
+        sed -i '' -e '/^DATABRICKS_APP_NAME=/d' .env.local
         echo "DATABRICKS_APP_NAME=$app_name" >> .env.local
         export DATABRICKS_APP_NAME="$app_name"
     fi
